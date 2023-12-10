4f79cd28c872de8e9305f5c4726e2da193a66393
Peter <ppkrauss@gmail.com>
2023-12-10 11:46:13 -0300

    add indexed cases

--- src/step80benchmark-p1.sql
+++ src/step80benchmark-p1.sql
@@ -4,9 +4,15 @@ EXPLAIN ANALYZE select  count(*) from optim.consolidated_data where afa_id betwe
 -- Planning Time: 0.102 ms to 0.108 ms
 -- Execution Time: 100.534 ms to 118.573 ms
 
+-- AFTER INDEXING, `ALTER TABLE optim.consolidated_data ADD PRIMARY KEY ( afa_id )`
+-- Execution Time: 0.707 ms!  1/100 of the non-indexed
+
 EXPLAIN ANALYZE select  count(*) from optim.consolidated_data where geom  &&  Box2D('0103000020E61000000100000005000000CD96D9EBA64A43C0D936D35206310EC0CD96D9EBA64A43C0C17F26385B230EC033AA45306F4943C0C17F26385B230EC033AA45306F4943C0D936D35206310EC0CD96D9EBA64A43C0D936D35206310EC0'::geometry);
 -- Planning Time: 0.159 ms to 0.163 ms
 -- Execution Time: 208.712 ms to 211.013 ms
 
-SELECT 'afa_id  ~2x faster tham BBOX, and advantage of no geometrytype need, only pure SQL' as conclusion;
- 
+-- AFTER INDEXING, `CREATE INDEX idx_geom_consolidated_data ON optim.consolidated_data USING gist (geom)`
+-- Execution Time: 0.754 ms to 1.575 ms
+
+SELECT 'afa_id non-indexed ~2x faster tham BBOX, and advantage of no geometrytype need, only pure SQL' as conclusion;
+SELECT 'afa_id indexed no very faster tham BBOX, but advantage of no geometrytype and less index cost' as conclusion;
